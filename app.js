f7df4f9c156bcc53197c2cc38cc4dcbc1c5c5a07
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-05-22 18:42:55 +0200

    add second dice

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ GAME RULES:
 
 // if I use (Math.random() * 6).toFixed() instead of Math.floor(Math.random() * 6), it gives string and it doesn't calculate scores
 // gamePlaying is the state function
-let roundScore, activePlayer, scores, gamePlaying, lastDice;
+let roundScore, activePlayer, scores, gamePlaying, lastDice, lastDice2;
 init();
 
-document.querySelector('.dice').style.display = 'none';
+document.getElementById('dice-1').style.display = 'none';
+document.getElementById('dice-2').style.display = 'none';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if (gamePlaying) {
@@ -22,17 +23,24 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     let dice = Math.floor(Math.random() * 6) + 1;
     // alert('hurray!!!' + dice);
     // 2- display result on dice
-    let diceDom = document.querySelector('.dice');
+    let diceDom = document.getElementById('dice-1');
     diceDom.style.display = 'block';
     diceDom.src = './images/dice-' + dice + '.png';
 
-    if (lastDice === 6 && dice === 6) {
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+    // alert('hurray!!!' + dice);
+    // 2- display result on dice
+    let diceDom2 = document.getElementById('dice-2');
+    diceDom2.style.display = 'block';
+    diceDom2.src = './images/dice-' + dice2 + '.png';
+
+    if ((lastDice === 6 && dice === 6) || (lastDice2 === 6 && dice2 === 6)) {
       scores[activePlayer] = 0;
       document.getElementById('score-' + activePlayer).textContent = '0';
     } else if (dice !== 1) {
       // 3- update the round score if the random is not 1
       // add score
-      roundScore += dice;
+      roundScore += dice + dice2;
       // update current score part
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
@@ -41,6 +49,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     }
     // it should be at the end of
     lastDice = dice;
+    lastDice2 = dice2;
   }
 });
 
@@ -63,7 +72,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     if (scores[activePlayer] >= winningScore) {
       document.getElementById('name-' + activePlayer).textContent = 'winner!';
       // alert(activePlayer + ' is the winner of the game');
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
       gamePlaying = false;
@@ -85,7 +95,8 @@ function nextPlayer() {
   document.querySelector('.player-1-panel').classList.toggle('active');
 
   // make the dice invisible when it change the player
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 }
 
 // we are passing init as an argument, not calling it here. callback function
